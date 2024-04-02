f1a676581f675065e2361d7a7651c3b9ed3f4b79
OlgaCh <Chamina51@mail.ru>
2024-04-02 15:12:41 +0300

    initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+    this.setSubject = function(subjectName){
+        this.subject = subjectName;
+    }
+    this.addMarks = function(...marksToAdd){
+        if (!this.marks){
+            console.log("Студент отчислен");
+            return;
+        }
+        this.marks.push(...marksToAdd)
+    };
+    this.getAverage = function(){
+        if (!this.marks || !this.marks.length){
+            return 0;
+        }
+        const sum = this.marks.reduce((acc, item) => acc + item);
+        return sum / this.marks.length;
+    };
+    this.exclude = function(reason){
+        delete this.subject;
+        delete this.marks;
+        this.excluded = reason;
+    };
   
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
