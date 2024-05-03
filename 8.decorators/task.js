091a2e7555a8949a9b32f9535417e5329c45f1cd
OlgaCh <Chamina51@mail.ru>
2024-05-03 19:27:52 +0300

    initial commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,49 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
+    let cache =[];
+    function wrapper(...args){
+        const hash = md5(JSON.stringify(args));
+        let objectInCache = cache.find(item => item.hash === hash);
+        if(objectInCache){
+            console.log("Из кеша: " + objectInCache.value, cache);
+            return "Из кеша: " + objectInCache.value;
+        }
+        let result = func(...args);
+        cache.push({hash: hash, value: result});
+        if(cache.length > 5){
+            cache.shift();
+        }
+        console.log("Вычисляем: " + result, cache);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
   
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+    let timeoutId;
+    let count = 0;
+    let allCount = 0;
+    function wrapper(...args){
+        if(timeoutId) {
+            console.log("уже есть таймаут", args);
+            clearTimeout(timeoutId);
+            allCount ++;
+        }
+        if(!timeoutId){
+            console.log("первый сигнал", args);
+            func.apply(this, args);
+            count ++;
+        }
+        timeoutId = setTimeout(() => {
+            console.log("задержка больше 2000 млсек, сработал таймаут");
+            count++;
+            func.apply(this, args);
+            wrapper.count = count;
+        }, delay);
+        wrapper.allCount = allCount;
+    }
+    return wrapper;
   
 }
